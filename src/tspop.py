c8738b8a8f5303721206e117c1f4839de557e9cf
Georgia Tsambos <gtsambos@student.unimelb.edu.au>
2022-07-05 17:03:21 +1000

    Added summary stats

--- src/tspop.py
+++ src/tspop.py
@@ -18,13 +18,15 @@ class PopAncestry(object):
 	:vartype population: numpy.ndarray, dtype=np.int32
 	"""
 
-	def __init__(self, left, right, population, ancestor, child):
+	def __init__(self, left, right, population, ancestor, child,
+		sample_nodes, sequence_length):
 		self.left = np.array(left, dtype=np.float64)
 		self.right = np.array(right, dtype=np.float64)
 		self.population = np.array(population, dtype=np.int32)
 		self.ancestor = np.array(ancestor, dtype=np.int32)
-		self.child = np.array(child, dtype=np.int32)
+		self.sample = np.array(child, dtype=np.int32)
 		self.num_rows = self.num_rows()
+		
 		# Create the ancestry tables
 		self.ancestry_table = pd.DataFrame(
 		data = {
@@ -32,21 +34,32 @@ class PopAncestry(object):
 			'right': right,
 			'ancestor' : ancestor,
 			'population' : population,
-			'child' : child
+			'sample' : child
 		})
 		self.ancestry_table.sort_values(
-			by=['child','left'], inplace=True, ignore_index=True)
+			by=['sample','left'], inplace=True, ignore_index=True)
 		# Squashed table
 		self.squashed_table = self.squash_ancestry_tracts()
-		self.squashed_table = self.squashed_table[['child', 'population', 'left', 'right']]
-		# Raw table
-		self.ancestry_table = self.ancestry_table[['child', 'population', 'ancestor', 'left', 'right']]
+		# Reorder the raw table
+		self.ancestry_table = self.ancestry_table[['sample', 'left', 'right', 'ancestor', 'population']]
+
+		# Summary attributes. Some are just wrappers for the ts attributes -- needed?
+		self.ancestral_pops = list(set(self.squashed_table['population']))
+		self.num_ancestral_pops = len(self.ancestral_pops)
+		self.samples = sample_nodes
+		self.num_samples = len(sample_nodes)
+		self.ancestors = list(set(self.ancestry_table['ancestor']))
+		self.num_ancestors = len(self.ancestors)
+		self.total_genome_length = sequence_length * self.num_samples
+		self.coverage = self.calculate_coverage()
 
 	def __str__(self):
-		ret = "id\tleft\t\tright\t\tancestor\t\tpopulation\tchild\n"
-		for j in range(self.num_rows):
-			ret += "{}\t{:.8f}\t{:.8f}\t{}\t{}\t{}\n".format(
-				j, self.left[j], self.right[j], self.ancestor[j], self.population[j], self.child[j])
+		ret = """\n\nPopAncestry summary\n\n"""
+		ret += "Number of ancestral populations: \t{}\n".format(self.num_ancestral_pops)
+		ret += "Number of sample chromosomes: \t\t{}\n".format(self.num_samples)
+		ret += "Number of ancestors: \t\t\t{}\n".format(self.num_ancestors)
+		ret += "Total length of genomes: \t\t{:.6f}\n".format(self.total_genome_length)
+		ret += "Ancestral coverage: \t\t\t{:.6f}\n".format(self.coverage)
 		return ret[:-1]
 
 	def num_rows(self):
@@ -62,7 +75,7 @@ class PopAncestry(object):
 		"""
 		assert len(self.left) == len(self.right)
 		assert len(self.right) == len(self.population)
-		assert len(self.population) == len(self.child)
+		assert len(self.population) == len(self.sample)
 
 	def squash_ancestry_tracts(self):
 		"""
@@ -76,22 +89,26 @@ class PopAncestry(object):
 		new_population = []
 
 		for ind, row in self.ancestry_table.iterrows():
-			if ind > 0 and row['left']==new_right[-1] and row['population'] == new_population[-1] and row['child'] == new_sample[-1]:
+			if ind > 0 and row['left']==new_right[-1] and row['population'] == new_population[-1] and row['sample'] == new_sample[-1]:
 				new_right[-1] = row['right']
 			else:
-				new_sample.append(row['child'])
+				new_sample.append(row['sample'])
 				new_left.append(row['left'])
 				new_right.append(row['right'])
 				new_population.append(row['population'])
 				
 		squashed_ancestry_table = pd.DataFrame({
-			'child': [int(i) for i in new_sample],
+			'sample': [int(i) for i in new_sample],
 			'left' : new_left,
 			'right': new_right,
 			'population' : [int(p) for p in new_population]
 		})
 
 		return(squashed_ancestry_table)
+
+	def calculate_coverage(self):
+		lengths = self.squashed_table['right'] - self.squashed_table['left']
+		return np.sum(lengths)
 
 def pop_ancestry(ts, census_time):
 	"""
@@ -118,7 +135,9 @@ def _replace_parents_with_pops(ts, census_nodes):
 		right=ancestor_table.right,
 		ancestor=ancestor_table.parent,
 		population=[population_ids[n] for n in ancestor_table.parent],
-		child=ancestor_table.child
+		child=ancestor_table.child,
+		sample_nodes=ts.samples(), # May not be in the child field!
+		sequence_length=ts.sequence_length
 	)
 
 	return local_ancestry
--- tests/test_tspop.py
+++ tests/test_tspop.py
@@ -7,47 +7,47 @@ import pytest
 census_time = 20.5
 demography = msprime.Demography()
 demography.add_population(
-    name="SMALL", initial_size=200)
+	name="SMALL", initial_size=200)
 demography.add_population(
-    name="BIG", initial_size=500)
+	name="BIG", initial_size=500)
 demography.add_population(
-    name="ADMIX", initial_size=200)
+	name="ADMIX", initial_size=200)
 demography.add_population(
-    name="ANC", initial_size=500)
+	name="ANC", initial_size=500)
 demography.add_admixture(
-    time=20,
-    derived="ADMIX",
-    ancestral=["SMALL", "BIG"],
-    proportions=[0.5, 0.5]
+	time=20,
+	derived="ADMIX",
+	ancestral=["SMALL", "BIG"],
+	proportions=[0.5, 0.5]
 )
 demography.add_census(time=census_time)
 demography.add_population_split(
-    time=600,
-    derived=["SMALL", "BIG"],
-    ancestral="ANC"
+	time=600,
+	derived=["SMALL", "BIG"],
+	ancestral="ANC"
 )
 seq_length = 1e7
 ts_ex = msprime.sim_ancestry(
-    samples={"SMALL": 0, "BIG": 0, "ADMIX" : 2},
-    demography=demography,
-    random_seed=1008,
-    sequence_length=seq_length,
-    recombination_rate=3e-8
+	samples={"SMALL": 0, "BIG": 0, "ADMIX" : 2},
+	demography=demography,
+	random_seed=1008,
+	sequence_length=seq_length,
+	recombination_rate=3e-8
 )
 
 def test_ancestry_table():
-    t = tspop.PopAncestry(
-        left=[], right=[], population=[], ancestor=[], child=[])
+	t = tspop.PopAncestry(
+		left=[], right=[], population=[], ancestor=[], child=[])
 
 def test_ancestry_table_bad_input():
-    with pytest.raises(Exception):
-        tspop.PopAncestry(
-            left=[0], right=[], population=[], ancestor=[], child=[])
+	with pytest.raises(Exception):
+		tspop.PopAncestry(
+			left=[0], right=[], population=[], ancestor=[], child=[])
 
 def test_pop_ancestry():
-	pop_table = tspop.pop_ancestry(ts_ex, census_time)
-	print(pop_table)
+	p = tspop.pop_ancestry(ts_ex, census_time)
+	print(p)
 
 def test_squash_ancestry():
-    pop_table = tspop.pop_ancestry(ts_ex, census_time)
-    print(pop_table.squash_ancestry_tracts())
+	pop_table = tspop.pop_ancestry(ts_ex, census_time)
+	print(pop_table.squashed_table)
