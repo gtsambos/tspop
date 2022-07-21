ed3e5dde040fba927f7afa86f0107467baacbe6d
Georgia Tsambos <gtsambos@student.unimelb.edu.au>
2022-07-21 14:01:28 +1000

    Added some things needed for PyPi.

--- setup.py
+++ setup.py
@@ -4,9 +4,14 @@ from setuptools import setup
 with open("README.rst", "r") as fh:
 	long_description = fh.read()
 
+version = {}
+with open("src/_version.py") as fp:
+    exec(fp.read(), version)
+fp.close()
+
 setup(
 	name = 'tspop',
-	version = '0.0.1',
+	version = version['__version__'],
 	description = 'Extracts population-based ancestry from simulated tree sequence datasets.',
 	url = "https://github.com/gtsambos/tspop",
 	author = "Georgia Tsambos",
@@ -15,8 +20,8 @@ setup(
 	package_dir = {'' : 'src'},
 	classifiers = [
 		"Programming Language :: Python :: 3",
-		"Topic :: Scientific/Engineering :: Bioinformatics",
-		"Development Status :: Planning",
+		"Topic :: Scientific/Engineering :: Bio-Informatics",
+		"Development Status :: 1 - Planning",
 		"License :: OSI Approved :: MIT License",
 		"Intended Audience :: Science/Research",
 		"Natural Language :: English"
@@ -25,8 +30,8 @@ setup(
 	long_description_content_type = "text/x-rst",
 	install_requires = [
 		"tskit>=0.2.3",
-		"pandas>=1.0.0",
-		"numpy>=1.10.0"
+		"pandas>=1.2.0",
+		"numpy>=1.21.0"
 	],
 	extras_require = {
 		"dev" : ["pytest>=3.7",
